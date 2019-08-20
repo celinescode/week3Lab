64d67b39a85c282012e2da1708e9432622e0b39b
bolin ma <celinesmacbook@bolindeMacBook.local>
2019-08-20 22:41:05 +1000

    2nd

--- router.js
+++ router.js
@@ -1,5 +1,6 @@
 let express = require('express');
 let router = express.Router();
+let db = [];
 
 router.get('/', function(req, res){
    res.send('Welcome to FIT2095 Home Page');
@@ -7,6 +8,37 @@ router.get('/', function(req, res){
 router.get('/about', function(req, res){
    res.send('This page is about FIT2095');
 });
+
+//E.g: http://localhost:8080/newItem/Tv/20/1500
+//Object {id:348, name:’TV’, quantity:20, price:1500} should be saved in the DB
+router.get('/newItem/:name/:quantity/:price',function(req,res){
+    let item = req.params;
+    console.log(item);
+    // while (true) {
+    //     let idNum = Math.round(Math.random()*1000);
+    //     if (db.includes(idNum)===false) {
+    //         break;
+    //     }
+    // }
+    let idNum = Math.round(Math.random()*1000);
+    let newRec = {
+        id : idNum,
+        name: item.name,
+        quantity: parseInt(item.quantity),
+        price: parseInt(item.price)
+    }
+    db.push(newRec);
+    res.send(generateList());
+
+    console.log(db)
+});
+
+router.get('/listAllItems', function(req,res){
+
+
+
+});
+
 
 //export this router 
 module.exports = router;
--- server.js
+++ server.js
@@ -3,6 +3,14 @@ var app = express();
 let url = require('url');
 let db= [];
 let router = require('./router.js');
+
+function generateList() {
+    let st = 'Id  Name   Quantity Price  </br>';
+    for (let i = 0; i < db.length; i++) {
+        st += db[i].id + ' | ' + db[i].name + ' | ' + db[i].quantity + +db[i].price + '</br>';
+    }
+    return st;
+}
 //both router.js and server.js should be in same directory
 app.use('/', router);
 
