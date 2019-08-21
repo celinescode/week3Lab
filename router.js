d0466f2953411dce9d6485739f659c8147284056
bolin ma <celinesmacbook@bolindeMacBook.local>
2019-08-21 14:40:07 +1000

    3rd

--- router.js
+++ router.js
@@ -5,9 +5,6 @@ let db = [];
 router.get('/', function(req, res){
    res.send('Welcome to FIT2095 Home Page');
 });
-router.get('/about', function(req, res){
-   res.send('This page is about FIT2095');
-});
 
 //E.g: http://localhost:8080/newItem/Tv/20/1500
 //Object {id:348, name:’TV’, quantity:20, price:1500} should be saved in the DB
@@ -28,16 +25,50 @@ router.get('/newItem/:name/:quantity/:price',function(req,res){
         price: parseInt(item.price)
     }
     db.push(newRec);
-    res.send(generateList());
+    res.send("added to database")
+    //res.send(generateList());
 
     console.log(db)
 });
 
+function generateList() {
+    let st = 'Id  Name   Quantity  Price    Cost</br>';
+    for (let i = 0; i < db.length; i++) {
+        st += db[i].id + ' | ' + db[i].name + ' | ' + db[i].quantity + ' | '+db[i].price + ' | ' + db[i].quantity * db[i].price +'</br>';
+    }
+    return st;
+}
 router.get('/listAllItems', function(req,res){
-
-
+    res.send(generateList());
 
 });
+
+router.get('/deleteItem/:idNum', function(req,res){
+    let idNum = req.params.idNum;
+    let index = -1;
+    for(let i = 0; i < db.length; i++){
+        if(db[i].id == idNum){
+            index = i;
+            break;
+        }
+    }
+    if(index == -1){
+        res.send("<h1>Not Exist")
+    }else{
+        db.splice(index, 1);
+        res.send("Delete Item Successfully, current length of database is: " + db.length);
+    }   
+});
+
+
+router.get('/totalValue', function(req,res){
+    let sum = 0
+    db.forEach((element)=>{
+        sum += element.price * element.quantity
+    })
+    res.send("total value is " + sum)
+})
+
 
 
 //export this router 
--- server.js
+++ server.js
@@ -1,18 +1,13 @@
 var express = require('express');
 var app = express();
-let url = require('url');
-let db= [];
+// let url = require('url');
+// let db= [];
 let router = require('./router.js');
 
-function generateList() {
-    let st = 'Id  Name   Quantity Price  </br>';
-    for (let i = 0; i < db.length; i++) {
-        st += db[i].id + ' | ' + db[i].name + ' | ' + db[i].quantity + +db[i].price + '</br>';
-    }
-    return st;
-}
+
 //both router.js and server.js should be in same directory
 app.use('/', router);
 
-app.listen(8080);
-console.log("server is running")
+app.listen(8080,()=>{
+    console.log("server is running")
+});
